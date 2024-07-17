3cea44437cd342e7e156bcd763648fa992e4ea2f
Your Name <you@example.com>
2024-07-17 20:44:09 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using ManagedNativeWifi;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace WiFiManager
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                WiFiService wifiService = new WiFiService();
+                wifiService.connectBestWifiNetwork();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+
+    }
+}
---
+++ WiFiService.cs
@@ -0,0 +1,45 @@
+﻿using ManagedNativeWifi;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WiFiManager
+{
+    internal class WiFiService
+    {
+
+        
+        private AvailableNetworkPack getBestAvailableNetwork() {
+            var availabeNetworks = NativeWifi.EnumerateAvailableNetworks();
+            if(!availabeNetworks.Any())
+            {
+                throw new Exception("No wifi Network Available");
+            }
+            return availabeNetworks.Where(network => !string.IsNullOrEmpty(network.ProfileName)).
+                                    OrderByDescending(network => network.SignalQuality).
+                                    FirstOrDefault();
+        }
+
+        private bool connectBestNetwork()
+        {
+            AvailableNetworkPack bestNetwork = getBestAvailableNetwork();
+            bool isConnected = NativeWifi.ConnectNetwork(
+                      bestNetwork.Interface.Id,
+                      bestNetwork.ProfileName,
+                      bestNetwork.BssType,
+                      null);
+            return isConnected;
+        }
+
+        public void connectBestWifiNetwork()
+        {
+            connectBestNetwork();
+            Console.WriteLine("Wifi with best signal Connected");
+        }
+
+    }
+}
